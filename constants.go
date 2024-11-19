30b1c5dc20f0da20e67f42284500cc2fa48f246a
Moshe Good <moshe+github@squareup.com>
2024-11-19 16:13:33 -0500

    use md5sum as the hashing algo

--- constants.go
+++ constants.go
@@ -1,6 +1,7 @@
 package algos
 
 import (
+	"encoding/binary"
 	"hash"
 	"hash/fnv"
 )
@@ -9,3 +10,12 @@ var NumNodes = 10
 var NumLeases = 1000
 
 var Hasher hash.Hash64 = fnv.New64a()
+
+type md5Hasher struct {
+	hash.Hash
+}
+
+func (h *md5Hasher) Sum64() uint64 {
+	bytes := h.Sum(nil)
+	return binary.BigEndian.Uint64(bytes[:8])
+}
